70105c3eb0a29958d62a246eb822d724ea139fe7
0tickpulse <62788361+0tickpulse@users.noreply.github.com>
2023-07-12 00:07:07 +0200

    fix edit url docusaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,7 +46,7 @@ const config = {
 
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
-                    editUrl: "https://github.com/0tickpulse/mm-unofficial-beginner-guide/edit/master/",
+                    editUrl: "https://github.com/0tickpulse/mm-unofficial-beginner-guide/edit/main/",
                 },
                 blog: false,
                 theme: {

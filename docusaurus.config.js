5f1226f2ee8c3003b53f76aa342a196e7fea025c
0tickpulse <62788361+0tickpulse@users.noreply.github.com>
2023-07-11 10:41:06 +0200

    fix url docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
     favicon: "img/favicon.ico",
 
     // Set the production url of your site here
-    url: "https://0tickpulse.github.io/mm-unofficial-beginner-guide/",
+    url: "https://0tickpulse.github.io/",
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: "/",
+    baseUrl: "/mm-unofficial-beginner-guide/",
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.

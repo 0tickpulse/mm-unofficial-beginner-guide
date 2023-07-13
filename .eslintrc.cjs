7b0548e4f55158840880fda85dc95a0648a2223d
0tickpulse <62788361+0tickpulse@users.noreply.github.com>
2023-07-13 11:03:34 +0200

    remove undef from eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,4 +4,7 @@ module.exports = {
     parser: "@typescript-eslint/parser",
     plugins: ["@typescript-eslint"],
     root: true,
+    rules: {
+        "no-undef": "off", // TS Server is already checking for this
+    }
 };
